5ddcebc8c1904c30ab79fe7dc83201b5bc6fe4e7
Rikard Gustafsson <218735@skane.se>
2021-10-17 23:23:47 +0200

    Added front end client

---
+++ Waffler.Web/ClientApp/src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import SideBar from "./components/sidebar/SideBar";
+import Content from "./components/Content";
+import "./App.css";
+
+const App = () => {
+    const [sidebarIsOpen, setSidebarOpen] = useState(true);
+    const toggleSidebar = () => setSidebarOpen(!sidebarIsOpen);
+
+    return (
+        <Router>
+            <div className="App wrapper">
+                <SideBar toggle={toggleSidebar} isOpen={sidebarIsOpen} />
+                <Content toggleSidebar={toggleSidebar} sidebarIsOpen={sidebarIsOpen} />
+            </div>
+        </Router>
+    );
+};
+
+export default App;
---
+++ Waffler.Web/ClientApp/src/components/chart/TradeChart.js
@@ -0,0 +1,9 @@
+﻿import React from "react";
+import ApexCharts from 'apexcharts'
+import { series, options } from './SampleData'
+
+const TradeChart = () => (
+    <ApexCharts options={options} series={series} type="candlestick" height={350} />
+);
+
+export default TradeChart;
